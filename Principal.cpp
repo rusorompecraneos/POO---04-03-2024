a5afb6599e165265970ce133769a918331b7945f
rusorompecraneos <159219427+rusorompecraneos@users.noreply.github.com>
2024-03-10 20:38:53 -0500

    Update Principal.cpp
    Guardo el paquete "Principal" con dos errores posibles.

--- Principal.cpp
+++ Principal.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
-#include "./Salud.cpp"
+#include "../Persona.cpp"
 
 using namespace std;
 
-int main{
-    
-    class Inicio{
-
-    Persona persona1;
+class Inicio{
+    public: 
+    inicio(){}:
+    void init();
+};
+    void Inicio::init(){
+        Persona persona1;
         persona1.pedirDatos();
         persona1.mostrarDatos();
         persona1.calcularIMC();
         persona1.esMayorDeEdad();
-
-        return  0;
-
-    }
+        cout << "¿Es mayor de edad?" << (persona.mayorEdad() ? "Si" : "No") << endl;
 }
-
+    
+int main{
+    
+}
 
