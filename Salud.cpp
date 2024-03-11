839ee17e1b8cfafec7adfff43e59a7d67a1846c8
rusorompecraneos <159219427+rusorompecraneos@users.noreply.github.com>
2024-03-10 20:37:43 -0500

    Update Salud.cpp
    Guardo este archivo con el reto 3 completado, la implementacion de una clase hija que hereda metodos y atributos de la clase padre.

--- Salud.cpp
+++ Salud.cpp
@@ -5,17 +5,69 @@ using namespace std;
 
 class Persona
 {
-public:
+// Atributos pblicos
+private:
     string tipoDoc;
     int documento;
     string nombre;
     string apellido;
-    int peso;
-    int estatura;
-    int edad;
+    int peso, estatura, edad;
     string sexo;
 
+    public:
     Persona() {} // Constructor vacío
+
+    // Hacemos todos los set
+    void settipoDoc(string tipoDoc){
+        this -> tipoDoc = tipoDoc;
+    }
+    void setdocumento(int documento){
+        this -> documento = documento;
+    }
+    void setnombre(string nombre){
+        this -> nombre = nombre;
+    }
+    void setapellido(string apellido){
+        this -> apellido = apellido;
+    }
+    void setpeso(int peso){
+        this -> peso = peso;
+    }
+    void setestatura(int estatura){
+        this -> estatura = estatura;
+    }
+    void setedad(int edad){
+        this -> edad = edad; 
+    }
+    void setsexo(string sexo){
+        this -> sexo = sexo;
+    }
+
+    // Hacemos todos los get.
+    string gettipoDoc(){
+        return tipoDoc;
+    }
+    int getdocumento(){
+        return documento;
+    }
+    string getnombre(){
+        return nombre;
+    }
+    string getapellido(){
+        return apellido;
+    }
+    int getpeso(){
+        return peso;
+    }
+    int getestatura(){
+        return estatura;
+    }
+    int getedad(){
+        return edad;
+    }
+    string getsexo(){
+        return sexo;
+    }
     
     // Declaración de funciones
     void pedirDatos();
@@ -84,6 +136,53 @@ bool Persona::esMayorDeEdad()
     }
     return false;
 }
+
+class Empleado : Persona{
+    Persona user1;
+
+    // Atributos de la sub-clase.
+    public:
+    string cargo;
+    string departamento;
+    int valorHora;
+    float horasTrabajadas;
+
+    // Metodos de la sub-clase.
+    public:
+    void Empleado :: pedirDatos(){
+        cout << "Ingrese su cargo " << endl;
+        cin >> cargo;
+        cout << "Ingrese su departamento " << endl;
+        cin >> departamento;
+        cout << "Ingrese el valor por hora " << endl;
+        cin >> valorHora;
+        cout << "Ingrese las horas trabajadas " << endl;
+        cin >> horasTrabajadas;
+    }
+
+     void Empleado :: mostrarDatos(){
+        cout << "Su tipo de documento es " << user1.gettipoDoc() << endl;
+        cout << "Su documento es " << user1.getdocumento() << endl;
+        cout << "Su nombre es " << user1.getnombre() << endl;
+        cout << "Su apellido es " << user1.getapellido() << endl;
+        cout << "Su peso es " << user1.getpeso() << endl;
+        cout << "Su estatura es " << user1.getestatura() << endl;
+        cout << "Su edad es " << user1.getedad() << endl;
+        cout << "Su sexo es " << user1.getsexo() << endl;
+        cout << "Su cargo es " << cargo << endl;
+        cout << "Su departamento es " << departamento << endl;
+        cout << "El valor por sus horas trabajadas son " << valorHora << endl;
+        cout << "Sus horas trabajadas son " << horasTrabajadas <<  endl; 
+     }
+
+     void CalcularHonorarios(){
+        double honorarios = (valorHora*horasTrabajadas);
+        double reitica = (honorarios* 0.966)/100;
+        double honorariosReales = honorarios - reitica;
+     }
+
+};
+
 
 int main()
 {   Persona persona1;
